b8675f4947e7415f9dead20f64a6441c056158a3
Krysoo <82394014+Krysoo@users.noreply.github.com>
2021-11-21 20:16:55 +0100

    optimalization

--- LookForProgram.cs
+++ LookForProgram.cs
@@ -4,43 +4,38 @@ using Newtonsoft.Json;
 
 class LookForProgram : SelectLanguage {
     
-    public bool findFolder(string name, string dir, string lang) {
-        
-  
-        string[] foldersinDir = System.IO.Directory.GetDirectories(dir, "*", new EnumerationOptions {
-            IgnoreInaccessible = true,
-            RecurseSubdirectories = true
-        });
+    public bool findFolder(string name, string lang) {
+        string[] locations = {"C:/Program Files (x86)", "C:/Program Files", "c:/", "d:/"};
 
-        bool didFind = false;
-
-        foreach(string path in foldersinDir) {
+        foreach(string location in locations) {
             
-            if (path.Contains(name, StringComparison.OrdinalIgnoreCase)) {
+            string[] foldersinDir = System.IO.Directory.GetDirectories(location, "*", new EnumerationOptions {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true
+            });
 
-                Console.WriteLine(LoadJson(lang).findedFile + name + " ! " + LoadJson(lang).location  + path);
-                Console.WriteLine(LoadJson(lang).isRightPath);
-
-                char sciezka = Convert.ToChar(Console.ReadLine());
-                if (sciezka.Equals('y')) {
-                    didFind = true;
-                    Console.WriteLine(LoadJson(lang).isRightFile);
-                    char ifSure = Convert.ToChar(Console.ReadLine());
-                    if (ifSure.Equals('y')) {
-                        deleteDir delete = new deleteDir();
-                        return delete.getRidOfDirectory(path);
-                    } else return false;
+            //bool didFind = false;
+            foreach(string path in foldersinDir) {
+                if (path.Contains(name, StringComparison.OrdinalIgnoreCase)) {
+                    Console.WriteLine(LoadJson(lang).findedFile + name + " ! " + LoadJson(lang).location  + path);
+                    Console.WriteLine(LoadJson(lang).isRightPath);
+                    char sciezka = Convert.ToChar(Console.ReadLine());
+                    if (sciezka.Equals('y')) {
+                        //didFind = true;
+                        Console.WriteLine(LoadJson(lang).isRightFile);
+                        char ifSure = Convert.ToChar(Console.ReadLine());
+                        if (ifSure.Equals('y')) {
+                            deleteDir delete = new deleteDir();
+                            return delete.getRidOfDirectory(path);
+                        } else return false;
+                    }
                 }
             }
-        } //end of foreach
-        if (!didFind) {
-            Console.WriteLine(LoadJson(lang).didntFindFile);
-            findFolder(name, "d:/", lang);
         } return false;
     }
 
     public LookForProgram(string name, string language) {
-        if (findFolder(name, "c:/", language)) {
+        if (findFolder(name, language)) {
             Console.WriteLine(LoadJson(language).findedAndDeleted + name);  
         } else {
             Console.WriteLine(LoadJson(language).didntDeleteFile);
